f3230c293b35a66d4f5290ce26f3ac03367a1e88
codeCampWithSifat <sayedhossainsifat300@.com>
2022-07-21 07:04:35 +0600

    done by the mercy of allah

--- src/Pages/Shared/Navigation/Navigation.js
+++ src/Pages/Shared/Navigation/Navigation.js
@@ -25,7 +25,7 @@ const Navigation = () => {
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Doctor Portal
+           Bangladesh Doctors Consulation
           </Typography>
           {user?.email ? (
             <Box>
